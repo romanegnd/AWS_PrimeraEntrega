468fca5721b71578b7aa2552027ce8223656f000
Romane GOUINEAUD <romanegnd@gmail.com>
2024-11-11 23:38:57 -0600

    las update before git clone on ec2

--- test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-response = requests.get("http://127.0.0.1:5000/alumnos")
-try:
-    json_data = response.json()  # essaie de convertir la réponse en JSON
-    print("La réponse est un JSON :", json_data)
-except ValueError:
-    print("La réponse n'est pas un JSON.")
-
